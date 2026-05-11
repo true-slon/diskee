f7ba29c9e7bccccda02341735eaba92aa2c73964
magggggot <gladyshevmatvey@gmail.com>
2026-05-11 22:20:17 +0300

    push

---
+++ diskee-project/src/main/java/com/diskee/diskee_project/DiskeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.diskee.diskee_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiskeeProjectApplication.class, args);
+	}
+
+}
---
+++ diskee-project/src/test/java/com/diskee/diskee_project/DiskeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diskee.diskee_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
